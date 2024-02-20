b8c155551999ad428d77d43b991e2449357113bd
shital07 <shital0711@gmail.com>
2024-02-20 14:35:39 -0800

    junit maven (#2)

---
+++ src/test/java/TestSandbox.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestSandbox {
+
+    @Test
+    void assertTrueTest(){
+
+        assertTrue(true,"true is true");
+    }
+}
